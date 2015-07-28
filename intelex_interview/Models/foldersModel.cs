ce2d1604453e9bad1ada3ede7c37e0428a3c9c96
Ashwin <alagappan.pal@gmail.com>
2015-07-28 13:13:12 -0400

    controller and model
    added controller and model

---
+++ intelex_interview/Models/foldersModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace intelex_interview.Models
+{
+    /*
+     * validation from foldersModel for the LINQ class - structure
+     * 
+    */
+    [MetadataType(typeof(foldersModel))]
+    public partial class structure
+    { 
+        
+    }
+
+    /*
+     * Providing the validation required for the model
+     * 
+    */
+
+    [Bind(Exclude= "Id,Parent_Id")]
+    public class foldersModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Folder Title")]
+        [Required(ErrorMessage = "Please enter the folder title")]
+        public string Name { get; set; }
+
+        public int Parent_Id { get; set; }
+
+    }
+}
