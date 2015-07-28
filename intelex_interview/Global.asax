8ce8b67bbc6043158f4854e315cd051d7d62b7e6
Ashwin <alagappan.pal@gmail.com>
2015-07-28 11:46:54 -0400

    first commit

---
+++ intelex_interview/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="intelex_interview.MvcApplication" Language="C#" %>
---
+++ intelex_interview/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(intelex_interview.Startup))]
+namespace intelex_interview
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
